b3428776d36ce2ccc03336ad3068ac863e75ee93
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-07-23 16:07:04 +0600

    added payload for add expense

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,6 +1,31 @@
+import { useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { AppContext } from "../context/AppContext";
+
 const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+
+    setName("");
+    setCost("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="row">
         <div className="col-sm">
           <label for="name">Name</label>
@@ -9,6 +34,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           ></input>
         </div>
         <div className="col-sm">
@@ -18,6 +45,8 @@ const AddExpenseForm = () => {
             type="text"
             className="form-control"
             id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
           ></input>
         </div>
       </div>
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,6 +2,11 @@ import { createContext, useReducer } from "react";
 
 const AppReducer = (state, action) => {
   switch (action.type) {
+    case "ADD_EXPENSE":
+      return {
+        ...state,
+        expenses: [...state.expenses, action.payload],
+      };
     default:
       return state;
   }
