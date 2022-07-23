6cef1c929af8c69eed3d064168f615f26b073cc9
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-07-24 00:20:29 +0600

    spent so far

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,7 +1,16 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const ExpenseTotal = () => {
+  const { expenses } = useContext(AppContext);
+
+  const totalExpense = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-primary">
-      <span>Spent so far: £1000</span>
+      <span>Spent so far: £{totalExpense}</span>
     </div>
   );
 };
