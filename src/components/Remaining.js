c2dadbe904a3de75bf474063d391b38e1ced5c60
Jack Pritom Soren <jackpritomsoren@hotmail.com>
2022-07-25 00:43:25 +0600

    remaining calculating

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
+
 const Remaining = () => {
+  const { expenses, budget } = useContext(AppContext);
+
+  const totalExpense = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+  const alertType = totalExpense > budget ? "alert-danger" : "alert-success";
+
   return (
-    <div className="alert alert-success">
-      <span>Remaining: £1000</span>
+    <div className={`alert ${alertType}`}>
+      <span>Remaining: £{budget - totalExpense}</span>
     </div>
   );
 };
